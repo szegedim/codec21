d263ef123dc568bcd00693f05dc578f3cb4d9887
Schmied Enterprises LLC <hq@schmied.us>
2025-02-15 17:29:57 -0800

    work

---
+++ send_png/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+gcc -o generate_png.out generate_png.c -lavcodec -lavformat -lavutil -lswscale -lpng -lcurl 
+gcc -o send_png.out send_png.c -lssl -lcrypto -Wdeprecated-declarations
+gcc -o receive_png.out receive_png.c -lssl -lcrypto -Wdeprecated-declarations
